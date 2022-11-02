89f7ad80c04ef215d97427e53bcce0d234acc135
dnl-kg <88490384+dnl-kg@users.noreply.github.com>
2022-11-02 03:44:55 -0400

    Update loginform.js

--- loginform.js
+++ loginform.js
@@ -1,36 +1,29 @@
-import react from "react";
 import "./loginform.css"
+import React, { useState } from 'react';
 
 const LoginForm = () => {
 
+    const [email, setEmail] = useState('Email');
+    const [password, setPassword] = useState('')
     return(
             <div className="login">
-            <h1>login</h1>
-            <input type="text" placeholder="username"/>
-            <input type="password" placeholder="password"/>
+                <div className="loginForm">
+                <h1>Hello There!</h1>
+                <h3>Login Or Sign UP. Connect to your peers.</h3>
 
+                <input type="text" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+                <input type="password" placeholder = "Password"value={password} onChange={(e)=>setPassword(e.target.value)}/>
+                <div className="login-btn"> <button>Log in</button></div> 
+                <div className="row"><div className="divider"></div><span>OR</span><div className="divider"></div></div>
+                <div className="login-btn "> <button className="alt-btn">Sign in with Google</button></div> 
+                <div className="login-btn"> <button>Sign in with Google</button></div> 
 
+                <a href="#" className="forgotLink">Forgot Password</a>
 
+                <h3 className="signUp">Don't have an account <a href="#">Sign up</a></h3>
 
-            <div className="login-btn">Login</div> 
-            <div className="signup-btn">Signup</div>
-           
-           
-           
-            <p>Or Login using:</p>
-            <div className="alt-login">
-            // icons
-
+                </div>
             </div>
-           
-            <p>Or Sign up using:</p>
-            <div className="alt-signup">
-              //icons
-
-            </div>
-
-
-        </div>
         
         
     )
